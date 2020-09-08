58b37e7b564a0dc2bae2f6500ddffa7311e28e7d
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-08 21:37:13 +0700

    CT HIỂN THỊ N SỐ NGUYÊN TỐ

---
+++ Hien thi so nguyen to/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Hien_thi_so_nguyen_to
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("CT HIỂN THỊ N SỐ NGUYÊN TỐ ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Nhập n :");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write($"{n} số nguyên tố đầu tiên là :");
+            int index = 0;
+            for (int i = 2; i < 200; i++)
+            {
+                bool check = true;
+                for (int count = 2; count <= Math.Sqrt(i); count++)
+                {
+                    if (i % count == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    index = index + 1;
+                    if (index <= n)
+                    {
+                        Console.Write($"{i} ,");
+                    }
+                }
+            }
+        }
+    }
+}
